1b48d82c3b63acd1f6d241f6a2c82a7e4f8296f7
Jackson Hagen <jackson.d.hagen@gmail.com>
2022-07-21 11:23:23 -0600

    vue

---
+++ bookie.client/src/components/ScheduleSpreadsheet.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+
+<script>
+import VGrid from "@revolist/vue3-datagrid"
+export default {
+  setup() {
+    return {
+			columns: [
+				{
+					name: "Title",
+					prop: "title"
+				}
+			]
+		}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- bookie.client/src/pages/SchedulePage.vue
+++ bookie.client/src/pages/SchedulePage.vue
@@ -1,4 +1,12 @@
-<template></template>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <ScheduleSpreadsheet />
+      </div>
+    </div>
+  </div>
+</template>
 
 
 <script>
