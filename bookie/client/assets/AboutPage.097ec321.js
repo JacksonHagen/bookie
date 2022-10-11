dad0d92d9c28898a6d2344609e1f16ddb0dc6e72
JacksonHagen <jackson.d.hagen@gmail.com>
2022-10-11 16:54:58 -0600

    deploy

---
+++ bookie/client/assets/AboutPage.097ec321.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.f69d0857.js";import{o,b as t,d as s}from"./vendor.a541e2dd.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function d(_,i,p,u,f,h){return o(),t("div",n,c)}var b=e(a,[["render",d]]);export{b as default};
